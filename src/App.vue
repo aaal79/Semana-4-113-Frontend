739c3c7367b76480c62de4776560b09d51a5fe6e
aaal79 <aaal79@hotmail.com>
2020-12-20 19:13:32 -0500

    Carga del backend de prueba semana 4

---
+++ src/App.vue
@@ -0,0 +1,251 @@
+<template>
+<div>
+  <div v-if="estado">
+     <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+        <a class="navbar-brand " href="# ">
+            <img src="images/mision_tic_2022.png" width="40" height="40"
+            class="d-inline-block align-top " alt=" " loading="lazy "> SEMANA-4-113
+        </a>
+        <div class="collapse navbar-collapse justify-content-end " id="navbarNav ">
+            <ul class="navbar-nav ">
+                <li class="nav-item active ">
+                    <a class="nav-link " v-on:click="ingresar" href="# ">Login <span class="sr-only "></span></a>
+                    <!-- <a class="nav-link " v-on:click="ingresar" href="# ">Login <span class="sr-only ">(current)</span></a> -->
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1" class=""></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2" class=""></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: First slide"><title>Placeholder</title>
+                <rect
+                    width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Second slide"><title>Placeholder</title>
+                <rect
+                    width="100%" height="100%" fill="#666"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Third slide"><title>Placeholder</title>
+                <rect
+                    width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+      
+    <div>
+      <home/>
+    </div>
+    <footer class="page-footer bg-secondary text-white pt-4">
+
+        <!-- Footer Text -->
+        <div class="container-fluid text-center text-md-left">
+
+            <!-- Grid row -->
+            <div class="row">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">CONTÁCTENOS</h5>
+                    <p>Hessler Nevin Inampuez López, Neiva. 111-1111111 / hnil_frontend@gmail.com</p>
+                    <p>Ariel Alfonso Angarita Leon, Bucaramanga. 222-2222222 / aaal_analista@gmail.com </p>
+                    <p>David Johan Villamil Padilla, Bogotá. 333-3333333 / djvp_arquitecto@hotmail.com</p>
+                    <p>Juan Carlos Gómez, Medellin. 444-4444444 / jcg_backend@outlook.com</p>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mb-md-0 mb-3">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">ENLACE A GITHUB</h5>
+                    <label for="" >En el siguiente link encontrará el desarrollo del reto: 
+                    <a href="https://github.com/aaal79/semana-2-113.git"> Semana-4-113 en github
+                    </a>
+                </label>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Footer Text -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-1 bg-dark">Proyecto Final Ciclo 3 - MisionTIC 2020
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+  </div>
+  <div v-else>
+  <v-app id="app">
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <template>
+          <v-list-item @click="home">
+            <v-list-item-action>
+              <v-icon>home</v-icon>
+            </v-list-item-action>
+            <v-list-item-title>
+              Inicio
+            </v-list-item-title>
+          </v-list-item>
+        </template>
+        <template v-if="logueado">
+          <v-list-group>
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                <v-list-item-title>
+                  Menú
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item :to="{name: 'categoria'}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Categorías
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item :to="{name: 'articulo'}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Artículos
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </template> 
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="silver darken-3"
+      dark
+    >
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-3"
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="hidden-sm-and-down">Sistema</span>
+      </v-toolbar-title>      
+      <v-spacer></v-spacer>
+      <v-btn @click="salir()" icon v-if="logueado">
+        <v-icon>logout</v-icon> Salir
+      </v-btn>
+      <v-btn :to="{name: 'login'}" icon v-else>
+        <v-icon>apps</v-icon> Login
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container
+        fluid
+        fill-height
+      >
+        <v-slide-y-transition mode="out-in">
+          <router-view/>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+    <v-footer height="auto">
+      <v-layout justify-center>
+        <v-flex text-xs-center>
+          <v-card flat tile color="primary" class="white--text">
+            <v-card-text class="white--text pt-0">
+              Proyecto Final &copy;2020
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+  </div>
+  </div>
+</template>
+
+<script>
+import home from './components/home.vue'
+export default {
+  components: { home },
+  name: 'App',
+
+    data () {
+      return {
+      drawer: false,
+      estado:1
+      }
+    },
+
+  computed:{
+    logueado(){
+      return this.$store.state.usuario;
+    },
+  },
+  created(){
+    this.$store.dispatch("autoLogin");
+  },
+  methods:{
+    salir(){
+      this.$store.dispatch("salir");
+      this.estado=1
+    },
+
+    ingresar(){
+      this.estado=0
+    },
+
+    home(){
+      this.estado=1
+
+    }
+  }
+};
+</script>
---
+++ src/components/home.vue
@@ -0,0 +1,75 @@
+<template>
+<div>
+    <div class="container-fluid">
+        <div class="row mt-md-5 mt-sm-5 mt-xs-5">
+            <div v-for="(art,indice) of datos" :key="art.id" class="col-lg-6 col-xs-12 border">
+                <div v-if="indice<=3">
+                <div  class="d-flex justify-content-center align-items-center">
+
+                    <div class="p-3">
+                       <svg width="12em" height="12em" viewBox="0 0 16 16" class="bi bi-alarm-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                       <path fill-rule="evenodd" d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"
+                            />
+                        </svg>
+                    </div>
+
+                    <div class="p-2">
+                        <p>
+                            {{art.nombre}}
+                        </p>
+                    </div>
+
+                </div>
+                <div class="d-flex container-fluid justify-content-end pb-2 mt-n2">
+                    <button type="button" class="btn btn-outline-info"> <a href="" target="_blank">Info>></a></button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-center mb-5">
+            <div v-for="miembro in team" :key="miembro.codigo" class="col mt-5">
+                     <div class="card text-white bg-dark">
+                        <div class="d-flex justify-content-center p-2">
+                            <img :src="miembro.image">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">CASO DE EXITO</h5>
+                            <h5 class="card-title"> {{miembro.nombre}}</h5>
+                            <p class="card-text">{{miembro.descripcion}}
+                            </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+
+<script>
+import axios from 'axios';
+export default {
+     data() {
+        return {
+            datos: null,
+            team: [{
+                nombre: '<<Telefónica>>',
+                descripcion: 'Desde el área global de Supply Chain en Telefónica siempre nos hemos apoyado en la tecnología para mejorar nuestros procesos y lanzar nuevos proyectos. Y eso fue lo que nos ocurrió allá por 2018, cuando comenzamos a entender que ventajas nos traería el uso de la tecnología Blockchain. Cada uno alimenta con su información la vida de un equipo y la usa para optimizar sus operaciones. Conseguimos: Optimizar el inventario de equipos que tenemos, lo que nos lleva a algo muy importante optimizar el gasto, controlar el buen uso de nuestros equipos por todos los actores de la cadena, alertar sobre acciones que no sabíamos ni que ocurrían, adelantarnos a los problemas de los clientes para ofrecerles soluciones proactivas, Ser más sostenibles al mejorar la utilización del material que compramos. Todo con la seguridad, transparencia y fiabilidad que te da la tecnología Blockchain. "Difícilmente se hubiera conseguido de otra manera", afirma el gerente de operaciones.',
+                image:"images/telefonica.jpg"
+            }, {
+                nombre: '<<Lotería de Navidad basada en blockchain, una realidad>>',
+                descripcion: 'Hace prácticamente dos años fantaseábamos con la idea de una lotería nacional basada en una infraestructura tecnológica DLT. Pues ahora, de forma pionera en España, la administración de lotería El Negrito de Alicante se ha puesto manos a la obra y ha desarrollado la tecnología necesaria para gestionar las participaciones en el Sorteo de Navidad de a través de blockchain. El sorteo extraordinario de Navidad de la Lotería Nacional española no necesita mayor presentación, siendo uno de los sorteos con mayor tradición y más queridos del mundo. La novedad radica en la forma de tratar la gestión de las participaciones: la tecnología blockchain se pone al servicio de una lotería que será indiscutiblemente más segura. Para la gente que está al tanto de las evoluciones de la tecnología blockchain, la sorpresa será que hasta ahora apenas se había aplicado la DLT en el sector, y nunca en España. Sorprendente, sin duda, teniendo en cuenta las virtudes de la cadena de bloques a la hora de certificar transacciones de manera rápida, económica y segura.',
+                image:"images/loteria.jpg"
+            }, ]
+        };
+    },
+    mounted() {
+     axios.get('articulo/list')
+    .then(response => (this.datos = response.data));
+
+    },
+    
+}
+</script>
